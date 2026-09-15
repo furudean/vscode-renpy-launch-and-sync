13f5b3e4e1ed456e3a0aa36c2e7af18f75020aee
Merilynn Paisley <31383109+furudean@users.noreply.github.com>
2026-09-14 21:03:25 -0700

    pass --user-data-dir for tests

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "@vscode/test-cli"
+import { tmpdir } from "node:os"
+import { join } from "node:path"
 
 export default defineConfig({
-	files: "out/test/extension/**/*.test.js"
+	files: "out/test/extension/**/*.test.js",
+	launchArgs: [`--user-data-dir=${join(tmpdir(), "vscode-renpy-warp-test")}`]
 })
